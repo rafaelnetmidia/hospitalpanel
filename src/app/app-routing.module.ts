0be48cee10e405b34b19986e0e5e543c1e6a046f
rafaelnetmidia <32550511+rafaelnetmidia@users.noreply.github.com>
2024-11-14 12:20:02 -0300

    funcionario component adicionado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,14 @@ import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './components/home/home.component';
-import { HeaderComponent } from './components/header/header.component';
+import { FuncionarioListComponent } from './components/funcionario/funcionario-list/funcionario-list.component';
 
 const routes: Routes = [
   {
     path: '', component: NavComponent, 
     children: [
       {path: 'home',  component: HomeComponent},
-      {path: 'header', component: HeaderComponent}
+      {path: 'funcionarios', component: FuncionarioListComponent}
     ]
   }
 ];
---
+++ src/app/models/funcionario.ts
@@ -0,0 +1,7 @@
+export interface Funcionario {
+    id?: any;
+    nome: string;
+    cpf: string;
+    email: string;
+
+}
